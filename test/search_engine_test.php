253ccdd489f9a8498ddb5be167892babd13ca778
Csongor Halmai <csongor@halmai.hu>
2019-06-10 17:20:04 +1000

    improving tests for SearchEngine

--- test/search_engine_test.php
+++ test/search_engine_test.php
@@ -8,148 +8,80 @@
 	
 	use PHPUnit\Framework\TestCase;
 	
-	class ZendeskSearcherProxy extends ZendeskSearcher {
-		public function help() {
-			parent::help();
-		}
-		
-		public function getListSearchableFields() {
-			return parent::getListSearchableFields();
+	class SearchEngineProxy extends SearchEngine {
+		public function setDataDir($dir) {
+			$this->dataDir = $dir;
 		}
 	}
 	
-	class SequenceFeeder {
-		protected $array = [];
-		protected $cursor = 0;
+	final class SearchEngineTest extends TestCase {
 		
-		function __construct($array) {
-			$this->array = $array;
+		public function testBadJson() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/bad_json/");
+			$this->expectException(DataFileLoadErrorException::class);
+			$items = $searchEngine->search("users", "_id", 1);
 		}
 		
-		function getNext() {
-			$ret = $this->array[$this->cursor];
-			$this->cursor++;
-			return $ret;
+		public function testNonArrayJson() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/not_array_json/");
+			$this->expectException(DataFileLoadErrorException::class);
+			$items = $searchEngine->search("users", "_id", 1);
+		}
+		
+		public function testNoIdInArrayElementInJson() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/no_id_in_array/");
+			$this->expectException(DataFileLoadErrorException::class);
+			$items = $searchEngine->search("users", "_id", 1);
+		}
+		
+		public function testSearchOneUser() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/good_jsons/");
+			$items = $searchEngine->search("users", "_id", 3);
+			
+			$this->assertEquals(count($items), 1);
+			$this->assertEquals($items[0]->getAttrValue("_id"), 3);
+		}
+		
+		public function testSearchMoreUsers() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/good_jsons/");
+			$items = $searchEngine->search("users", "active", "true");
+			
+			$this->assertTrue(count($items) > 1);
+		}
+		
+		public function testSearchOneOrganization() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/good_jsons/");
+			$items = $searchEngine->search("organizations", "_id", 102);
+			
+			$this->assertEquals(count($items), 1);
+			$this->assertEquals($items[0]->getAttrValue("_id"), 102);
+		}
+		
+		public function testSearchOneTickets() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/good_jsons/");
+			$items = $searchEngine->search("tickets", "_id", "1a227508-9f39-427c-8f57-1b72f3fab87c");
+			
+			$this->assertEquals(count($items), 1);
+			$this->assertEquals($items[0]->getAttrValue("_id"), "1a227508-9f39-427c-8f57-1b72f3fab87c");
+		}
+		
+		public function testInvalidItemType() {
+			$searchEngine = new SearchEngineProxy();
+			$searchEngine->setDataDir(__DIR__. "/data/good_jsons/");
+			$items = $searchEngine->search("pink_unicorns", "age", "9999");
+			
+			$this->assertSame($items, []);
 		}
 	}
 	
-	final class ZendeskSearcherTest extends TestCase {
-		/*
-		public function testHelp(): void {
-			$paramFeeder = new CliFeeder;
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			
-			$zs->help();
-			
-			$this->expectOutputRegex("/Zendesk/");
-		}
-		
-		public function testGetListSearchableFields() {
-			$paramFeeder = new CliFeeder;
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			
-			$list = $zs->getListSearchableFields();
-			$this->assertRegexp("/\n_id.*\nexternal_id/s", $list);
-		}
-		
-		public function testRunHelp() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php", "--help"];
-			});
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			
-			$zs->run();
-			$this->expectOutputRegex("/Zendesk/");
-		}
-		
-		public function testRunSearchNotEnoughArguments() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php", "users"];
-			});
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			try {
-				$zs->run();
-			} catch (\Throwable $t) {
-				$this->assertEquals($t->getMessage(), "error.not_enough_arguments");
-				return;
-			}
-			
-			// if the above catch did not happen then the expected throw did not happen.
-			$this->assertEquals(1, 2);
-		}
-		
-		public function testRunSearchFromCommandLine() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php", "users", "_id", 6];
-			});
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			$zs->run();
-			$this->expectOutputRegex("/6/");
-		}
-		
-		public function testRunQuit() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php"];
-			});
-			
-			$sf = new SequenceFeeder(["quit"]);
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			$zs->setReader([$sf, "getNext"]);
-			
-			$zs->run();
-			$this->assertEquals(1, 1); // we just assert that the code has returned.
-		}
-		
-		public function testRunInteractiveSearch() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php"];
-			});
-			
-			$sf = new SequenceFeeder(["1", "2", "_id", "436bf9b0-1147-4c0a-8439-6f79833bff5b", "quit"]);
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			$zs->setReader([$sf, "getNext"]);
-			
-			$zs->run();
-			$this->expectOutputRegex("/ \[_id\] => 436bf9b0-1147-4c0a-8439-6f79833bff5b/");
-		}
-		
-		public function testRunInteractiveListOfFields() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php"];
-			});
-			
-			$sf = new SequenceFeeder(["2", "quit"]);
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			$zs->setReader([$sf, "getNext"]);
-			
-			$zs->run();
-			$this->expectOutputRegex("/\n_id.*\nexternal_id/s");
-		}
-		*/
-		
-		
-		public function testRunInteractiveOrganizations() {
-			$paramFeeder = new CliFeeder(function() {
-				return ["program.php"];
-			});
-			
-			$sf = new SequenceFeeder(["1", "organizations", "_id", "102", "quit"]);
-			
-			$zs = new ZendeskSearcherProxy($paramFeeder);
-			$zs->setReader([$sf, "getNext"]);
-			
-			$zs->run();
-			$output = $this->getActualOutput();
-			
-			$this->assertRegexp("/_id.*external_id/s", $output);
-		}
-	}
+	
 	
 		
 
